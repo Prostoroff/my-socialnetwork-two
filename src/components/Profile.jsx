e018448586025fc1f10ede5c65ff434633a98e6c
Prostoroff <77617380+Prostoroff@users.noreply.github.com>
2021-02-18 18:50:41 +0500

    my first commit

---
+++ src/components/Profile.jsx
@@ -0,0 +1,28 @@
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwux95DR0PH-RmW8D5EhlrfY13CyHeqF5-fQ&usqp=CAU" />
+      </div>
+      <div>
+        ava + description
+      </div>
+      <div>
+        My posts
+        <div>
+          New posts
+        </div>
+        <div>
+          <div>
+            post 1
+          </div>
+          <div>
+            post 2
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Profile;
